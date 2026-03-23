33f08f05b105de83fb367405bb1d150f65b65a77
prosports <prosports100plus@gmail.com>
2026-03-23 15:32:56 +0530

    Create ModelGenerationViewModel.kt for 3D model generation

---
+++ app/src/main/java/com/example/yourapp/ModelGenerationViewModel.kt
@@ -0,0 +1,9 @@
+import androidx.lifecycle.ViewModel
+
+class ModelGenerationViewModel : ViewModel() {
+    // LiveData and other properties for model generation
+
+    fun generate3DModel(parameters: Any) {
+        // Logic to generate 3D model
+    }
+}
